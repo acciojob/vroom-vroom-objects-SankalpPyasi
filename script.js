db6ce1af6abb6089565e6aa7fbeb167ec5d86225
SankalpPyasi <sankalppyasi6@gmail.com>
2025-02-20 12:08:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,32 @@
-// Complete the js code
-
+// Car constructor function
 function Car(make, model) {
-	this.make = make;
-	this.model = model;
+    this.make = make;
+    this.model = model;
 }
 
+// Method to get car details
+Car.prototype.getDetails = function() {
+    return `Car: ${this.make} ${this.model}`;
+};
+
+// SportsCar constructor function inheriting from Car
 function SportsCar(make, model, topSpeed) {
-	Car.call(this, make, model);
-	this.topSpeed = topSpeed;
+    Car.call(this, make, model); // Call parent constructor
+    this.topSpeed = topSpeed;
 }
 
-// class Car{
-// 	constructor(make, model)
-// 	{
-// 		this.make = make;
-// 		this.model = model;
-// 	}	
-// }
-
-Car.prototype.getMakeModel = function() 
-{
-	return ${this.make} ${this.model};
-}
-
-
-// class SportsCar extends Car{	
-// 	constructor(make, model, topSpeed)
-// 	{
-// 		super(name, model);
-// 		this.topSpeed = topSpeed;
-// 	}
-// }
-
+// Inheriting Car prototype
 SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
 
+// Method to get sports car details
+SportsCar.prototype.getSportsCarDetails = function() {
+    return `${this.getDetails()}, Top Speed: ${this.topSpeed} km/h`;
+};
 
-SportsCar.prototype.getTopSpeed = function() 
-{
-	return this.topSpeed;
-}
+// Example usage
+const myCar = new Car("Toyota", "Corolla");
+console.log(myCar.getDetails());
 
-
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+const mySportsCar = new SportsCar("Ferrari", "488", 330);
+console.log(mySportsCar.getSportsCarDetails());
