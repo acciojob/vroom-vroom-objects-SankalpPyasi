862bb30eaf9e94019a1a8a3828819e17a23fa498
SankalpPyasi <sankalppyasi6@gmail.com>
2025-02-22 18:23:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,8 @@
-// Car constructor function
-function Car(make, model) {
-    this.make = make;
-    this.model = model;
-}
+// Complete the js code
+function Car(make, model) {}
 
-// Method to get car details
-Car.prototype.getDetails = function() {
-    return `Car: ${this.make} ${this.model}`;
-};
+function SportsCar(make, model, topSpeed) {}
 
-// SportsCar constructor function inheriting from Car
-function SportsCar(make, model, topSpeed) {
-    Car.call(this, make, model); // Call parent constructor
-    this.topSpeed = topSpeed;
-}
-
-// Inheriting Car prototype
-SportsCar.prototype = Object.create(Car.prototype);
-SportsCar.prototype.constructor = SportsCar;
-
-// Method to get sports car details
-SportsCar.prototype.getSportsCarDetails = function() {
-    return `${this.getDetails()}, Top Speed: ${this.topSpeed} km/h`;
-};
-
-// Example usage
-const myCar = new Car("Toyota", "Corolla");
-console.log(myCar.getDetails());
-
-const mySportsCar = new SportsCar("Ferrari", "488", 330);
-console.log(mySportsCar.getSportsCarDetails());
+// Do not change the code below
+window.Car = Car;
+window.SportsCar = SportsCar;
